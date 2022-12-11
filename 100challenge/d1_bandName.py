15bfc0b5a65bf23e091c1fd0fdc586bd06884605
challanger95 <67339800+challanger95@users.noreply.github.com>
2022-12-11 21:18:02 +0100

    Create d1_bandName.py
    First step to the greatness!

---
+++ 100challenge/d1_bandName.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator. \nWhat's name of the city you grew up in?")
+city = input()
+print("What's your pet name?")
+petName = input()
+print("Your band name could be: " + city + " " + petName)
