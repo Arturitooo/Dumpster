56e68aab8ac1f67da2ff1153dc5d14d92e7f7502
challanger95 <67339800+challanger95@users.noreply.github.com>
2023-01-08 16:03:58 +0100

    day 14 of bootcamp, first solid project
    higher or lower game, about number of followers of these celebrities

---
+++ 100challenge/d14_databaseForHLG.py
@@ -0,0 +1,168 @@
+#d14_databaseForHLG
+
+logo = """    __  ___       __             
+   / / / (_)___ _/ /_  ___  _____
+  / /_/ / / __ `/ __ \/ _ \/ ___/
+ / __  / / /_/ / / / /  __/ /    
+/_/ ///_/\__, /_/ /_/\___/_/     
+   / /  /____/_      _____  _____
+  / /   / __ \ | /| / / _ \/ ___/
+ / /___/ /_/ / |/ |/ /  __/ /    
+/_____/\____/|__/|__/\___/_/     
+"""
+
+vs = """ 
+ _    __    
+| |  / /____
+| | / / ___/
+| |/ (__  ) 
+|___/____(_)
+
+"""
+
+data = [
+    {
+        'name': 'Instagram',
+        'follower_count': 346,
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 215,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Ariana Grande',
+        'follower_count': 183,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Dwayne Johnson',
+        'follower_count': 181,
+        'description': 'Actor and professional wrestler',
+        'country': 'United States'
+    },
+    {
+        'name': 'Selena Gomez',
+        'follower_count': 174,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kylie Jenner',
+        'follower_count': 172,
+        'description': 'Reality TV personality and businesswoman and Self-Made Billionaire',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'follower_count': 167,
+        'description': 'Reality TV personality and businesswoman',
+        'country': 'United States'
+    },
+    {
+        'name': 'Lionel Messi',
+        'follower_count': 149,
+        'description': 'Footballer',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Beyoncé',
+        'follower_count': 145,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Neymar',
+        'follower_count': 138,
+        'description': 'Footballer',
+        'country': 'Brasil'
+    },
+    {
+        'name': 'National Geographic',
+        'follower_count': 135,
+        'description': 'Magazine',
+        'country': 'United States'
+    },
+    {
+        'name': 'Justin Bieber',
+        'follower_count': 133,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Taylor Swift',
+        'follower_count': 131,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kendall Jenner',
+        'follower_count': 127,
+        'description': 'Reality TV personality and Model',
+        'country': 'United States'
+    },
+    {
+        'name': 'Jennifer Lopez',
+        'follower_count': 119,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Nicki Minaj',
+        'follower_count': 113,
+        'description': 'Musician',
+        'country': 'Trinidad and Tobago'
+    },
+    {
+        'name': 'Nike',
+        'follower_count': 109,
+        'description': 'Sportswear multinational',
+        'country': 'United States'
+    },
+    {
+        'name': 'Khloé Kardashian',
+        'follower_count': 108,
+        'description': 'Reality TV personality and businesswoman',
+        'country': 'United States'
+    },
+    {
+        'name': 'Miley Cyrus',
+        'follower_count': 107,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Katy Perry',
+        'follower_count': 94,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kourtney Kardashian',
+        'follower_count': 90,
+        'description': 'Reality TV personality',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kevin Hart',
+        'follower_count': 89,
+        'description': 'Comedian and actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Ellen DeGeneres',
+        'follower_count': 87,
+        'description': 'Comedian',
+        'country': 'United States'
+    },
+    {
+        'name': 'Real Madrid CF',
+        'follower_count': 86,
+        'description': 'Football club',
+        'country': 'Spain'
+    }
+]
