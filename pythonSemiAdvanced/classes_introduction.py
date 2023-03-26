7a6e4d6b08a81c39dfccd3e6f85b83a3433ccc67
challanger95 <67339800+challanger95@users.noreply.github.com>
2023-03-26 18:12:03 +0200

    Update classes_introduction.py
    update

--- pythonSemiAdvanced/classes_introduction.py
+++ pythonSemiAdvanced/classes_introduction.py
@@ -44,3 +44,65 @@ def show_cake_info(cake):
  
 show_cake_info(cake1)
 show_cake_info(cake2)
+
+
+## classes and classes atributes
+
+class Car:
+    
+    def __init__(self, brand, model, isAirBagOK, isPaintingOK,isMechanicOK):
+        self.brand = brand
+        self.model = model
+        self.isAirBagOK = isAirBagOK
+        self.isPaintingOK = isPaintingOK
+        self.isMechanicOK = isMechanicOK
+
+car_01 = Car('Seat','Ibiza',True, True, True)
+
+
+car_02 = {
+'carBrand' : 'Opel',
+'carModel' : 'Corsa',
+'carIsAirBagOK' : True,
+'carIsPaintingOK' : False,
+'carIsMechanicOK' : True}
+
+print(car_01)
+
+def IsCarDamaged(aCar):
+    return not  (aCar ['carIsAirBagOK'] and aCar['carIsPaintingOK'] and aCar['carIsMechanicOK'])
+
+def ClassIsCarDamaged(aCar):
+    return not  (aCar.brand, aCar.model, aCar.isAirBagOK, aCar.isPaintingOK, aCar.isMechanicOK)
+
+
+cars = [car_01, car_02]
+
+print(ClassIsCarDamaged(car_01))
+print(IsCarDamaged(car_02))
+
+#class instance - element created with class
+
+#homework https://www.udemy.com/course/python-dla-srednio-zaawansowanych/learn/lecture/14790372#overview
+
+class Cake():
+    def __init__(self, name, kind, taste, additives, filling = ""):
+        self.name = name
+        self.kind = kind
+        self.taste = taste
+        self.additives = additives
+        self.filling = filling
+
+cake01 = Cake('Vanilla Cake','cake', 'vanilla', ['chocolade', 'nuts'], 'cream')
+cake02 = Cake('Chocolade Muffin','muffin', 'chocolade', ['chocolade'], '')
+cake03 = Cake('Super Sweet Maringue','meringue', 'very sweet', [], '')
+ 
+bakery_offer = []
+bakery_offer.append(cake01)
+bakery_offer.append(cake02)
+bakery_offer.append(cake03)
+ 
+print("Today in our offer:")
+for c in bakery_offer:
+    print("{} - ({}) main taste: {} with additives of {}, filled with {}".format(
+        c.name, c.kind, c.taste, c.additives, c.filling))
