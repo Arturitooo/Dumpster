7cbd2c298e70fbe460365a127a6b5e5dd2f56221
challanger95 <67339800+challanger95@users.noreply.github.com>
2023-04-16 21:50:59 +0200

    day39_step3Finished

--- 100challenge/d39_40_cheapFlight/data_manager.py
+++ 100challenge/d39_40_cheapFlight/data_manager.py
@@ -1,6 +1,6 @@
 import requests
 
-SHEETY_GET = "https://api.sheety.co/b6d9b81549a734f41b4a4ab2e1f6f667/flightDeals/prices"
+SHEETY_API = "https://api.sheety.co/b6d9b81549a734f41b4a4ab2e1f6f667/flightDeals/prices"
 
 
 class DataManager:
@@ -9,6 +9,23 @@ class DataManager:
         self.destination_data = {}
 
     def get_destination_data(self):
-        response = requests.get(url=SHEETY_GET)
-        destination_data = response.json()['prices']
-        return destination_data
+        response = requests.get(url=SHEETY_API)
+        self.destination_data = response.json()['prices']
+        return self.destination_data
+
+# 6. In the DataManager Class make a PUT request and use the row id
+# from sheet_data to update the Google Sheet with the IATA codes. (Do this using code).
+# HINT: Remember to check the checkbox to allow PUT requests in Sheety.
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_item = {
+                'price': {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_API}/{city['id']}",
+                json=new_item
+            )
+            print(response.text)
