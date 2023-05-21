03d31f2141a46b206bca6a795d38e846ce147f26
challanger95 <67339800+challanger95@users.noreply.github.com>
2023-05-21 22:49:49 +0200

    section 7 done

---
+++ Django 4  Full-Stack course/Python/module.py
@@ -0,0 +1,9 @@
+def useful_func():
+    print("This is very useful function")
+
+def UsefulClass():
+    def __init__(self, message):
+        self.message = message
+
+    def report(self):
+        print(self.message)
---
+++ Django 4  Full-Stack course/Python/mypackage/submodel.py
@@ -0,0 +1,2 @@
+def sub_func():
+    print("this is cool function coming from sub folder")
