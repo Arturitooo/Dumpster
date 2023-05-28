10d640ff5a122532a5ec865e332317831f981cbb
challanger95 <67339800+challanger95@users.noreply.github.com>
2023-05-28 11:19:43 +0200

    Django redirects basics
    quick revision, small changes

--- Django4Full-Stack course/DJANGO/section9/venv/my_site/first_app/urls.py
+++ Django4Full-Stack course/DJANGO/section9/venv/my_site/first_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:num_page>/', views.num_page_view),
     path('<str:topic>/', views.news_view),
     # it allows to create dynamic adress
-    path('<int:num1>/<int:num2>', views.add_view)
+    path('<int:num1>/<int:num2>', views.add_view),
 ]
