febdfff867d478daf45099870f1c77d304e4d911
challanger95 <67339800+challanger95@users.noreply.github.com>
2023-06-04 22:09:58 +0200

    section 11 - working on databases and models

--- Django4Full-Stack course/DJANGO/section11/s11_app/real_app/models.py
+++ Django4Full-Stack course/DJANGO/section11/s11_app/real_app/models.py
@@ -3,5 +3,8 @@ from django.db import models
 # Create your models here.
 class Patient(models.Model):
     first_name = models.CharField(max_length=30)
-    first_name = models.CharField(max_length=30)
     age = models.IntegerField()
+
+    def __str__(self):
+
+        return f"The patient name is: {self.first_name} nad he is {self.age} years old"
--- Django4Full-Stack course/DJANGO/section11/s11_app/s11_app/settings.py
+++ Django4Full-Stack course/DJANGO/section11/s11_app/s11_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'real_app.apps.RealAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
