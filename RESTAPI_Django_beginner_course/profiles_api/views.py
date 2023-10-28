65a7e45cbf8232032f5bc24acdf47670bef93494
Arturitooo <arturito.python@gmail.com>
2023-10-28 20:12:55 +0200

    Newly created ProfileFeed API also some fun with permissions

--- RESTAPI_Django_beginner_course/profiles_api/views.py
+++ RESTAPI_Django_beginner_course/profiles_api/views.py
@@ -7,6 +7,8 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 from . import serializers
 from . import models
 from . import permissions
@@ -114,3 +116,19 @@ class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
 
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles CRU profile feed items"""
+
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticated,
+    )
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
