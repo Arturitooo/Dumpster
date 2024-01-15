030db69c71dd835c4a746b38d00c19f5b336eb13
Arturitooo <arturito.python@gmail.com>
2024-01-15 23:07:26 +0100

    js basics

---
+++ js/fizzbuzz.js
@@ -0,0 +1,17 @@
+var output = [];
+var count = 1;
+
+function fizzBuzz(){
+    if ((count % 5 === 0 ) && (count % 3 === 0)){
+          output.push("FizzBuzz");
+        } else if (count % 5 === 0 ){
+          output.push("Buzz");
+        } else if (count % 3 === 0 ){
+          output.push("Fizz");
+        } else if ((count % 5 !== 0 ) && (count % 3 !== 0)){
+          output.push(count);
+        }
+
+    count++;
+    return output;
+}
