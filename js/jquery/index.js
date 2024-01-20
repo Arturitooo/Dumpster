62f8ac8215068e0b6302f0571aae4f8e530a28c6
Arturitooo <arturito.python@gmail.com>
2024-01-20 15:29:38 +0100

    basics of jQuery and js event listeners & querySelectors

---
+++ js/jquery/index.js
@@ -0,0 +1,9 @@
+$("h1").addClass("big-title margin-50"); // setting value of it when 2 values in brackets
+$("h1").css("color"); // getting value of it when 1 value in brackets
+
+$("h1").text("bye bye");
+$("h1").removeClass("margin-50");
+$("button").text("Don't click me");
+$("button").html("<b>Click</b>");
+
+console.log($("a").attr("href"));
