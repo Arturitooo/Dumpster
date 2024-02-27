e46f19f4655c0f7cf9e849ec397462f6e801950d
Arturitooo <arturito.python@gmail.com>
2024-02-27 20:44:52 +0100

    Adding navbar and some simple styling

--- fullstack/frontend/src/App.js
+++ fullstack/frontend/src/App.js
@@ -3,16 +3,23 @@ import {Routes, Route, Router} from 'react-router-dom';
 import { Home } from './components/Home';
 import { About } from './components/About';
 import { Create } from './components/Create';
+import { Navbar } from './components/Navbar';
 
 
 function App() {
+  const navbarWidth = 240;
   return (
     <div className="App">
-      <Routes>
-        <Route path="" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/create" element={<Create/>} />
-      </Routes>
+      <Navbar 
+        drawerWidth={navbarWidth}
+        content = {
+          <Routes>
+            <Route path="" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/create" element={<Create/>} />
+          </Routes>
+        }
+      />
     </div>
   );
 }
