1aace0d0b42ff484ecf7af3e4994124cc0c1ad5b
Arturitooo <arturito.python@gmail.com>
2024-02-29 20:28:43 +0100

    Basic API preparation

--- fullstack/backend/core/settings.py
+++ fullstack/backend/core/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.security.",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -55,7 +54,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000/",
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = "core.urls"
